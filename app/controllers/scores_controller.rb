f1c7f41978b1bb9a6b331b3cecbecffac1e065ea
iona-b <ionabrabender@gmail.com>
2020-08-20 14:51:35 -0700

    added to scores controller

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,30 @@
 class ScoresController < ApplicationController
-    def create
-        byebug
+    def show
+        score = Score.find_by(id: params[:id])
+        render json: score
     end
-
+    def index
+        scores = Score.all
+        render json: scores
+    end
+    def create
+        score = Score.create(score_params)
+        if score.valid?
+            render json: score
+        else
+            render json: {requirePatch: true}
+        end 
+    end
+    def update
+        score = Score.find_by(id: params[:id])
+        if score.update(score: params[:score])
+            render json: score
+        else
+            render json: {error: 'Could not update'}
+        end
+    end
+    private 
+    def score_params
+        params.permit(:score, :user_id, :level_id)
+    end 
 end
