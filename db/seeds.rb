dbb260dad31cb6f4baf2940516998e1fdea9ba81
iona-b <ionabrabender@gmail.com>
2020-08-20 21:29:00 -0700

    added level 1 and added sound effects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Level.destroy_all
-Score.destroy_all
+# User.destroy_all
+# Level.destroy_all
+# Score.destroy_all
 
 
-user1 = User.create(username: 'karamo', password: '123')
-user2 = User.create(username: 'jonathon', password: '123')
-user3 = User.create(username: 'antoni', password: '123')
-user4 = User.create(username: 'bobby', password: '123')
-user5 = User.create(username: 'tan', password: '123')
+# user1 = User.create(username: 'karamo', password: '123')
+# user2 = User.create(username: 'jonathon', password: '123')
+# user3 = User.create(username: 'antoni', password: '123')
+# user4 = User.create(username: 'bobby', password: '123')
+# user5 = User.create(username: 'tan', password: '123')
 
-level1 = Level.create(level_name: 'Easy', difficulty_level: 'Easy')
-level2 = Level.create(level_name: 'Medium', difficulty_level: 'Medium')
-level3 = Level.create(level_name: 'Hard', difficulty_level: 'Hard')
+# level1 = Level.create(level_name: 'Easy', difficulty_level: 'Easy')
+# level2 = Level.create(level_name: 'Medium', difficulty_level: 'Medium')
+# level3 = Level.create(level_name: 'Hard', difficulty_level: 'Hard')
+# level4 = Level.create(level_name: 'Level1', difficulty_level: 'Easy')
 
-score1 = Score.create(score: 19, user: User.first, level: level1)
-score2 = Score.create(score: 77, user: User.second, level: level1)
-score3 = Score.create(score: 26, user: User.third, level: level2)
-score4 = Score.create(score: 40, user: User.fourth, level: level3)
-score5 = Score.create(score: 100, user: User.fifth, level: level3)
+# score1 = Score.create(score: 19, user: User.first, level: level1)
+# score2 = Score.create(score: 77, user: User.second, level: level1)
+# score3 = Score.create(score: 26, user: User.third, level: level2)
+# score4 = Score.create(score: 40, user: User.fourth, level: level3)
+# score5 = Score.create(score: 100, user: User.fifth, level: level3)
