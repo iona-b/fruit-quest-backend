1ae9d9a674c8832a85cfc3e22d7246e5bb06fabe
iona-b <ionabrabender@gmail.com>
2020-08-20 17:31:34 -0700

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :scores
-  resources :levels
-  resources :users
+  # resources :levels
+  resources :users, except: [:edit, :update]
   post "/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
